7fdc6ab602736cc7a61f44518d9014d858c4d86c
vitalii-minchuk <mcmin4doutsch@gmail.com>
2025-04-11 20:58:54 +0200

    add fondy testing tequest

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
+import crypto from "crypto";
 import router from "./routes";
+import axios from "axios";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -11,8 +13,8 @@ app.use(express.json());
 app.use(router);
 
 const sendTelegramMessage = async (message: string) => {
-  const botToken = "YOUR_BOT_TOKEN";
-  const chatId = "YOUR_CHAT_ID";
+  const botToken = "111111";
+  const chatId = "111111";
   const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
   const body = {
@@ -41,7 +43,39 @@ const sendTelegramMessage = async (message: string) => {
   }
 };
 
+const testFondy = async () => {
+  const fondyPassword = "test";
+  const params = {
+    order_id: "12366",
+    merchant_id: "1396424",
+    amount: "5400",
+    order_desc: "test description",
+    currency: "UAH",
+  };
+  const sortedParams = Object.keys(params).sort((a, b) => a.localeCompare(b));
+  const stringBody = (sortedParams as Array<keyof typeof params>)
+    .map((el) => params[el])
+    .join("|");
+
+  function generateSignature(string: string) {
+    const sha1 = crypto.createHash("sha1");
+    sha1.update(`${fondyPassword}|${string}`);
+    return sha1.digest("hex");
+  }
+
+  const { data } = await axios.post("https://pay.fondy.eu/api/checkout/url/", {
+    request: {
+      ...params,
+      signature: generateSignature(stringBody),
+    },
+  });
+  console.log(generateSignature(stringBody));
+  console.log("first", data);
+};
+
 app.get("/", (req, res) => {
+  testFondy();
+  // sendTelegramMessage("ddd");
   res.send("Hello World!!!!!!!!!!");
 });
 
